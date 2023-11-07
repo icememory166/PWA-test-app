0c1a1f59d6dddb9cfd4de2ef1f83759167e45193
icememory166 <53908539+icememory166@users.noreply.github.com>
2023-11-07 15:06:35 +0100

    update

--- serviceworker.js
+++ serviceworker.js
@@ -1,22 +1,34 @@
-// Perform install steps
-let CACHE_NAME = 'my-cache';
-let urlsToCache = [
-    'assets/css/style.css',
-    'assets/css/style2.css',    
-    'dist/img/boat.jpg',
-    'dist/img/cat.jpg',
-    'assets/js/index.js',
-    'airvent.html'
+const staticPhoneStore = "app"
+const assets = [
+  "/",
+  "/index.html",
+  "/airvent.html",
+  "/css/style.css",
+  "/css/style2.css",
+  "/js/index.js",
+  "dist/img/boat.jpg",
+  "dist/img/cat.jpg",
+  "/images/icon-72x72.jpg",
+  "/images/icon-96x96.jpg",
+  "/images/128x128.jpg",
+  "/images/144x144.jpg",
+  "/images/152x152.jpg",
+  "/images/192x192.jpg",
+  "/images/384x384.jpg",
+  "/images/512x512.jpg",
+]
 
-    ];
-
-self.addEventListener('install', function(event) {
-// Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-            console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-        })
-    );
-});
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticPhoneStore).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
