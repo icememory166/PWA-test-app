915111dff46897e7103fed0b36cbd067eede98f4
icememory166 <53908539+icememory166@users.noreply.github.com>
2023-11-07 14:12:57 +0100

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,18 @@
-let btn = document.getElementById("btn")
-console.log("trat")
+const btn = document.getElementById("btn")
+const counter = document.getElementById("counter")
+const img = document.getElementById("boat")
+
+let num = 0
+
+counter.style.display = "none"
 
 btn.addEventListener("click", function() {
-    let counter = document.createElement("p")
-    counter.textContent("Click count is: ")
-    console.log("test")
+    counter.style.display = "block"
+    num += 1
+    counter.textContent = (`Click count is: ${num}`)
+    if(num >= 5 && num < 10){
+        img.src = "dist/img/cat.jpg"
+    } else if (num >= 10){
+        window.location.href = "airvent.html"
+    }
 })
